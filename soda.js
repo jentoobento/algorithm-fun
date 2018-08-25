098582392d95ddf362c5e6757a8fd8b70799779e
jennifer <41801164+jentoobento@users.noreply.github.com>
2018-08-25 16:17:53 -0700

    Update soda.js

--- soda.js
+++ soda.js
@@ -5,13 +5,14 @@ and nothing if neither applies
 */
 
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log('fizzbuzz')
-  } else if (i % 3 === 0) {
-    console.log('fizz')
-  } else if (i % 5 === 0) {
-    console.log('buzz')
-  } else {
-    console.log(i)
-  }
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz')
+    } else if (i % 3 === 0) {
+        console.log('fizz')
+    } else if (i % 5 === 0) {
+        console.log('buzz')
+    } else {
+        console.log(i)
+    }
 }
+
